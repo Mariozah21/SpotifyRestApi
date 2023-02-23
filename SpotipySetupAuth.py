ef9892efd6b1e1e5210b8fcb63b14d44403fa4b3
Mario Zahorjan <mariozah21@gmail.com>
2023-02-23 14:57:47 +0100

    Revert "Setup created"
    This reverts commit 4da2ee51d61b3e7156f01bcb85921317fe3be214.

--- SpotipySetupAuth.py
+++
@@ -1,8 +0,0 @@
-import spotipy
-from spotipy.oauth2 import SpotifyClientCredentials
-
-cid = '1855ccc24e144ff4a8daef9848c4780f'
-secret = '2a001b3e61894450ad7ae6bc763477a1'
-
-client_credentials_manager = SpotifyClientCredentials(client_id=cid,client_secret=secret)
-sp = spotipy.Spotify(client_credentials_manager = client_credentials_manager)
